0058ece187a4be7b17bf61c811dbfb5e95e9dfe5
ChichoRD <chichord2005@hotmail.com>
2022-05-17 17:48:04 +0200

    Removed Namespace

--- SaveManager.cs
+++ SaveManager.cs
@@ -25,243 +25,240 @@ using UnityEngine.Rendering.Universal;
 using ShadowQuality = UnityEngine.ShadowQuality;
 using ShadowResolution = UnityEngine.Rendering.Universal.ShadowResolution;
 
-namespace SettingsSystem.DataStorage
+public enum DocType
 {
-    public enum DocType
+    PlayerData,
+    GeneralData,
+    CustomizationData,
+}
+
+public static class SaveManager
+{
+    private static PLayerData pLayerData = new();
+    private static GeneralData generalData = new();
+    private static CustomizationData customizationData = new();
+
+    private static readonly string fileExtension = ".xml";
+
+    public static Action OnSaved { get; private set; }
+    public static Action OnLoaded { get; private set; }
+    public static Action OnDeleted { get; private set; }
+
+    public static PLayerData PLayerData { get => pLayerData ?? new(); set => pLayerData = value; }
+    public static GeneralData GeneralData { get => generalData ?? new(); set => generalData = value; }
+    public static CustomizationData CustomizationData { get => customizationData ?? new(); set => customizationData = value; }
+
+    private static readonly string DataPath = Application.persistentDataPath;
+    private static string StreamPath(string fileName, string dataPath) => dataPath + "/" + fileName + fileExtension;
+    private static string StreamPath(DocType fileType, string dataPath) => dataPath + "/" + GetFileName(fileType) + fileExtension;
+
+    public static void Save<T>(T dataToSave, string fileName)
     {
-        PlayerData,
-        GeneralData,
-        CustomizationData,
+        var serializer = new XmlSerializer(typeof(T));
+        var stream = new FileStream(StreamPath(fileName, DataPath), FileMode.Create);
+        serializer.Serialize(stream, dataToSave);
+        stream.Close();
+
+        OnSaved?.Invoke();
+        Debug.Log("Saved");
     }
 
-    public static class SaveManager
+    public static void Save<T>(T dataToSave, DocType fileType)
     {
-        private static PLayerData pLayerData = new();
-        private static GeneralData generalData = new();
-        private static CustomizationData customizationData = new();
+        var serializer = new XmlSerializer(typeof(T));
+        var stream = new FileStream(StreamPath(fileType, DataPath), FileMode.Create);
+        serializer.Serialize(stream, dataToSave);
+        stream.Close();
 
-        private static readonly string fileExtension = ".xml";
+        OnSaved?.Invoke();
+        Debug.Log("Saved");
+    }
 
-        public static Action OnSaved { get; private set; }
-        public static Action OnLoaded { get; private set; }
-        public static Action OnDeleted { get; private set; }
-
-        public static PLayerData PLayerData { get => pLayerData ?? new(); set => pLayerData = value; }
-        public static GeneralData GeneralData { get => generalData ?? new(); set => generalData = value; }
-        public static CustomizationData CustomizationData { get => customizationData ?? new(); set => customizationData = value; }
-
-        private static readonly string DataPath = Application.persistentDataPath;
-        private static string StreamPath(string fileName, string dataPath) => dataPath + "/" + fileName + fileExtension;
-        private static string StreamPath(DocType fileType, string dataPath) => dataPath + "/" + GetFileName(fileType) + fileExtension;
-
-        public static void Save<T>(T dataToSave, string fileName)
+    public static void Load<T>(out T dataToLoad, string fileName) where T : class, new()
+    {
+        if (File.Exists(StreamPath(fileName, DataPath)))
         {
             var serializer = new XmlSerializer(typeof(T));
-            var stream = new FileStream(StreamPath(fileName, DataPath), FileMode.Create);
-            serializer.Serialize(stream, dataToSave);
+            var stream = new FileStream(StreamPath(fileName, DataPath), FileMode.Open);
+            dataToLoad = serializer.Deserialize(stream) as T;
             stream.Close();
 
-            OnSaved?.Invoke();
-            Debug.Log("Saved");
+            OnLoaded?.Invoke();
+            Debug.Log("Loaded");
+            return;
         }
 
-        public static void Save<T>(T dataToSave, DocType fileType)
+        dataToLoad = new T();
+    }
+
+    public static void Load<T>(out T dataToLoad, DocType fileType) where T : class, new()
+    {
+        if (File.Exists(StreamPath(fileType, DataPath)))
         {
             var serializer = new XmlSerializer(typeof(T));
-            var stream = new FileStream(StreamPath(fileType, DataPath), FileMode.Create);
-            serializer.Serialize(stream, dataToSave);
+            var stream = new FileStream(StreamPath(fileType, DataPath), FileMode.Open);
+            dataToLoad = serializer.Deserialize(stream) as T;
             stream.Close();
 
-            OnSaved?.Invoke();
-            Debug.Log("Saved");
+            OnLoaded?.Invoke();
+            Debug.Log("Loaded");
+            return;
         }
 
-        public static void Load<T>(out T dataToLoad, string fileName) where T : class, new()
-        {
-            if (File.Exists(StreamPath(fileName, DataPath)))
-            {
-                var serializer = new XmlSerializer(typeof(T));
-                var stream = new FileStream(StreamPath(fileName, DataPath), FileMode.Open);
-                dataToLoad = serializer.Deserialize(stream) as T;
-                stream.Close();
-
-                OnLoaded?.Invoke();
-                Debug.Log("Loaded");
-                return;
-            }
-
-            dataToLoad = new T();
-        }
-
-        public static void Load<T>(out T dataToLoad, DocType fileType) where T : class, new()
-        {
-            if (File.Exists(StreamPath(fileType, DataPath)))
-            {
-                var serializer = new XmlSerializer(typeof(T));
-                var stream = new FileStream(StreamPath(fileType, DataPath), FileMode.Open);
-                dataToLoad = serializer.Deserialize(stream) as T;
-                stream.Close();
-
-                OnLoaded?.Invoke();
-                Debug.Log("Loaded");
-                return;
-            }
-
-            dataToLoad = new T();
-        }
-
-        public static T Load<T>(string fileName) where T : class, new()
-        {
-            if (File.Exists(StreamPath(fileName, DataPath)))
-            {
-                T data;
-
-                var serializer = new XmlSerializer(typeof(T));
-                var stream = new FileStream(StreamPath(fileName, DataPath), FileMode.Open);
-                data = serializer.Deserialize(stream) as T;
-                stream.Close();
-
-                OnLoaded?.Invoke();
-                Debug.Log("Loaded");
-                return data;
-            }
-
-            return new T();
-        }
-
-
-        public static T Load<T>(DocType fileType) where T : class, new()
-        {
-            if (File.Exists(StreamPath(fileType, DataPath)))
-            {
-                T data;
-
-                var serializer = new XmlSerializer(typeof(T));
-                var stream = new FileStream(StreamPath(fileType, DataPath), FileMode.Open);
-                data = serializer.Deserialize(stream) as T;
-                stream.Dispose();
-
-                OnLoaded?.Invoke();
-                Debug.Log("Loaded");
-                return data;
-            }
-
-            return new T();
-        }
-
-
-        public static void DeleteSavedData(string fileName)
-        {
-            if (File.Exists(StreamPath(fileName, DataPath)))
-            {
-                File.Delete(StreamPath(fileName, DataPath));
-
-                OnDeleted?.Invoke();
-                Debug.Log("Deleted data");
-            }
-        }
-
-        public static void DeleteSavedData(DocType fileType)
-        {
-            if (File.Exists(StreamPath(fileType, DataPath)))
-            {
-                File.Delete(StreamPath(fileType, DataPath));
-
-                OnDeleted?.Invoke();
-                Debug.Log("Deleted data");
-            }
-        }
-
-        public static void LoadAndSave<T>(ref T dataToLoadAndSave, DocType docType, Action<T> codeBetween = null) where T : class, new()
-        {
-            dataToLoadAndSave = Load<T>(docType);
-
-            codeBetween?.Invoke(dataToLoadAndSave);
-
-            Save(dataToLoadAndSave, docType);
-        }
-
-        public static T SaveAndLoad<T>(T dataToLoadAndSave, DocType docType, Action codeBetween = null) where T : class, new()
-        {
-            Save(dataToLoadAndSave, docType);
-
-            codeBetween?.Invoke();
-
-            return Load<T>(docType);
-        }
-
-
-        public static T GetFieldValue<T>(object obj, string fieldName)
-        {
-            if (obj == null)
-                throw new ArgumentNullException("obj");
-
-            var field = obj.GetType().GetField(fieldName, BindingFlags.Public |
-                                                          BindingFlags.NonPublic |
-                                                          BindingFlags.Instance);
-
-            if (field == null)
-                throw new ArgumentException("fieldName", "No such field was found.");
-
-            if (!typeof(T).IsAssignableFrom(field.FieldType))
-                throw new InvalidOperationException("Field type and requested type are not compatible.");
-
-            return (T)field.GetValue(obj);
-        }
-
-        private static string GetFileName(DocType docType)
-        {
-            return docType switch
-            {
-                DocType.PlayerData => nameof(PLayerData),
-                DocType.GeneralData => nameof(GeneralData),
-                DocType.CustomizationData => nameof(CustomizationData),
-                _ => nameof(GeneralData)
-            };
-        }
+        dataToLoad = new T();
     }
 
-    [System.Serializable]
-    public sealed class GeneralData
+    public static T Load<T>(string fileName) where T : class, new()
     {
+        if (File.Exists(StreamPath(fileName, DataPath)))
+        {
+            T data;
 
+            var serializer = new XmlSerializer(typeof(T));
+            var stream = new FileStream(StreamPath(fileName, DataPath), FileMode.Open);
+            data = serializer.Deserialize(stream) as T;
+            stream.Close();
+
+            OnLoaded?.Invoke();
+            Debug.Log("Loaded");
+            return data;
+        }
+
+        return new T();
     }
 
-    [System.Serializable]
-    public sealed class PLayerData
+
+    public static T Load<T>(DocType fileType) where T : class, new()
     {
+        if (File.Exists(StreamPath(fileType, DataPath)))
+        {
+            T data;
 
+            var serializer = new XmlSerializer(typeof(T));
+            var stream = new FileStream(StreamPath(fileType, DataPath), FileMode.Open);
+            data = serializer.Deserialize(stream) as T;
+            stream.Dispose();
+
+            OnLoaded?.Invoke();
+            Debug.Log("Loaded");
+            return data;
+        }
+
+        return new T();
     }
 
-    [System.Serializable]
-    public sealed class CustomizationData
+
+    public static void DeleteSavedData(string fileName)
     {
-        // Video Settings
-        public Vector2Int screenResolution = new(0, 0);
-        public FullScreenMode screenMode = FullScreenMode.ExclusiveFullScreen;
-        public int frameRate = -1;
-        public byte vSyncCount = 1;
+        if (File.Exists(StreamPath(fileName, DataPath)))
+        {
+            File.Delete(StreamPath(fileName, DataPath));
 
-        //Audio Settings
-        public float rawMasterVolume = 1f;
-        public float rawSFXVolume = 1f;
-        public float rawMusicVolume = 1f;
-        public bool enableTextSpeechSound = true;
-
-        //General Settings
-        public float cameraShake = 1f;
-        public float fieldOfView = 90f;
-        public float sensitivity = 1f;
-        public bool visualTips = true;
-
-        //Graphics Settings
-        public byte qualityLevel = 2;
-        public bool postProcessing = true;
-        public ShadowQuality shadowQuality = ShadowQuality.All;
-        public ShadowResolution shadowResolution = ShadowResolution._2048;
-        public byte shadowCascadesCount = 4;
-        public float shadowDistance = 100f;
-        public MsaaQuality msaaQuality = MsaaQuality._2x;
-        public float renderScale = 1f;
+            OnDeleted?.Invoke();
+            Debug.Log("Deleted data");
+        }
     }
+
+    public static void DeleteSavedData(DocType fileType)
+    {
+        if (File.Exists(StreamPath(fileType, DataPath)))
+        {
+            File.Delete(StreamPath(fileType, DataPath));
+
+            OnDeleted?.Invoke();
+            Debug.Log("Deleted data");
+        }
+    }
+
+    public static void LoadAndSave<T>(ref T dataToLoadAndSave, DocType docType, Action<T> codeBetween = null) where T : class, new()
+    {
+        dataToLoadAndSave = Load<T>(docType);
+
+        codeBetween?.Invoke(dataToLoadAndSave);
+
+        Save(dataToLoadAndSave, docType);
+    }
+
+    public static T SaveAndLoad<T>(T dataToLoadAndSave, DocType docType, Action codeBetween = null) where T : class, new()
+    {
+        Save(dataToLoadAndSave, docType);
+
+        codeBetween?.Invoke();
+
+        return Load<T>(docType);
+    }
+
+
+    public static T GetFieldValue<T>(object obj, string fieldName)
+    {
+        if (obj == null)
+            throw new ArgumentNullException("obj");
+
+        var field = obj.GetType().GetField(fieldName, BindingFlags.Public |
+                                                      BindingFlags.NonPublic |
+                                                      BindingFlags.Instance);
+
+        if (field == null)
+            throw new ArgumentException("fieldName", "No such field was found.");
+
+        if (!typeof(T).IsAssignableFrom(field.FieldType))
+            throw new InvalidOperationException("Field type and requested type are not compatible.");
+
+        return (T)field.GetValue(obj);
+    }
+
+    private static string GetFileName(DocType docType)
+    {
+        return docType switch
+        {
+            DocType.PlayerData => nameof(PLayerData),
+            DocType.GeneralData => nameof(GeneralData),
+            DocType.CustomizationData => nameof(CustomizationData),
+            _ => nameof(GeneralData)
+        };
+    }
+}
+
+[System.Serializable]
+public sealed class GeneralData
+{
+
+}
+
+[System.Serializable]
+public sealed class PLayerData
+{
+
+}
+
+[System.Serializable]
+public sealed class CustomizationData
+{
+    // Video Settings
+    public Vector2Int screenResolution = new(0, 0);
+    public FullScreenMode screenMode = FullScreenMode.ExclusiveFullScreen;
+    public int frameRate = -1;
+    public byte vSyncCount = 1;
+
+    //Audio Settings
+    public float rawMasterVolume = 1f;
+    public float rawSFXVolume = 1f;
+    public float rawMusicVolume = 1f;
+    public bool enableTextSpeechSound = true;
+
+    //General Settings
+    public float cameraShake = 1f;
+    public float fieldOfView = 90f;
+    public float sensitivity = 1f;
+    public bool visualTips = true;
+
+    //Graphics Settings
+    public byte qualityLevel = 2;
+    public bool postProcessing = true;
+    public ShadowQuality shadowQuality = ShadowQuality.All;
+    public ShadowResolution shadowResolution = ShadowResolution._2048;
+    public byte shadowCascadesCount = 4;
+    public float shadowDistance = 100f;
+    public MsaaQuality msaaQuality = MsaaQuality._2x;
+    public float renderScale = 1f;
 }
